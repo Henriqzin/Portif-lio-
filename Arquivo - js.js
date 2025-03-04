efba62c29c76c53f7c426c6e2e5860980bc03caa
Henriqzin <hickpereira2019@gmail.com>
2025-03-04 13:16:53 -0300

    Add files via upload

---
+++ Arquivo - js.js
@@ -0,0 +1,45 @@
+document.querySelectorAll('.menu-desktop a, .btn-contato a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajuste conforme necessário
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const abrirMenu = document.querySelector(".abrir-menu");
+    const menuMobile = document.querySelector(".menu-mobile");
+    const overlayMenu = document.querySelector(".overlay-menu");
+
+    function fecharMenu() {
+        menuMobile.classList.remove("abrimenu");
+        overlayMenu.style.display = "none";
+        document.removeEventListener("click", fecharMenu);
+    }
+
+    abrirMenu.addEventListener("click", function(event) {
+        // Impede que o clique no botão se propague e feche o menu imediatamente
+        event.stopPropagation();
+        
+        // Se o menu já estiver aberto, fecha-o
+        if (menuMobile.classList.contains("abrimenu")) {
+            fecharMenu();
+        } else {
+            // Abre o menu
+            menuMobile.classList.add("abrimenu");
+            overlayMenu.style.display = "block";
+            // Após abrir, adiciona o listener que fecha o menu com clique em qualquer lugar
+            setTimeout(() => {
+                document.addEventListener("click", fecharMenu);
+            }, 0);
+        }
+    });
+});
